1a504254d2fd485341e7274dffd5c6167e80d738
Jason Chu <jjchu@uwaterloo.ca>
2018-05-26 07:27:38 -0400

    Added map activity. Renamed view names in RequestActivity. Added onClickListener to the button in RequestActivity.

--- airatmandroid/app/src/main/java/ca/thecashmen/airatmandroid/RequestActivity.java
+++ airatmandroid/app/src/main/java/ca/thecashmen/airatmandroid/RequestActivity.java
@@ -1,14 +1,27 @@
 package ca.thecashmen.airatmandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.View;
+import android.widget.Button;
 
 public class RequestActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
+
+        button = findViewById(R.id.requestMoneyButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RequestActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
