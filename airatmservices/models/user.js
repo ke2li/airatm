17c249cb62e2cf4e539b8713be3f1b9bd980a293
ke2li <li.kyle25@gmail.com>
2018-05-26 14:14:34 -0400

    Removed unnecessary code

--- airatmservices/models/user.js
+++ airatmservices/models/user.js
@@ -3,61 +3,50 @@ const mongoose = require("mongoose");
 const user = mongoose.Schema({
 	firstName: {
 		type: String,
-		required: true,
 	},
 
 	lastName: {
 		type: String,
-		required: true,
 	},
 
 	email: {
 		type: String,
-		required: true,
 	},
 
 	password: {
 		type: String,
-		required: true,
 	},
 
 	reputation: {
 		type: Number,
 		default: 0,
-		required: true,
 	},
 
 	verified: {
 		type: Boolean,
 		default: false,
-		required: true,
 	},
 
 	accBalance: {
 		type: Number,
 		default: 0,
-		required: true,
 	},
 
 	cashOnHand: {
 		type: Number,
 		default: 0,
-		required: false,
 	},
 
 	longitude: {
 		type: Number,
-		required: false,
 	},
 
 	latitude: {
 		type: Number,
-		required: true,
 	},
 
 	online: {
 		type: Boolean,
-		required: true,
 	},
 });
 
