21a24d7d1748827441fff516a045c21459503a2a
David <davidzhoucanada@gmail.com>
2018-05-26 15:45:11 -0400

    whitespace changes

--- airatmandroid/app/src/main/java/ca/thecashmen/airatmandroid/MapActivity.java
+++ airatmandroid/app/src/main/java/ca/thecashmen/airatmandroid/MapActivity.java
@@ -55,11 +55,11 @@ public class MapActivity extends AppCompatActivity
     }
 
     @Override
-    public void onPause() {
+    public void onPause(){
         super.onPause();
 
         //stop location updates when Activity is no longer active
-        if (mFusedLocationClient != null) {
+        if(mFusedLocationClient != null){
             mFusedLocationClient.removeLocationUpdates(mLocationCallback);
         }
     }
@@ -75,19 +75,19 @@ public class MapActivity extends AppCompatActivity
         mLocationRequest.setFastestInterval(120000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(this,
+        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(ContextCompat.checkSelfPermission(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)
                     == PackageManager.PERMISSION_GRANTED) {
                 //Location Permission already granted
-                mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+                mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                        mLocationCallback, Looper.myLooper());
                 mGoogleMap.setMyLocationEnabled(true);
-            } else {
+            } else{
                 //Request Location Permission
                 checkLocationPermission();
             }
-        }
-        else {
+        } else{
             mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
             mGoogleMap.setMyLocationEnabled(true);
         }
@@ -97,12 +97,12 @@ public class MapActivity extends AppCompatActivity
         @Override
         public void onLocationResult(LocationResult locationResult) {
             List<Location> locationList = locationResult.getLocations();
-            if (locationList.size() > 0) {
+            if(locationList.size() > 0){
                 //The last location in the list is the newest
                 Location location = locationList.get(locationList.size() - 1);
                 Log.i("MapActivity", "Location: " + location.getLatitude() + " " + location.getLongitude());
                 mLastLocation = location;
-                if (mCurrLocationMarker != null) {
+                if(mCurrLocationMarker != null){
                     mCurrLocationMarker.remove();
                 }
 
@@ -120,14 +120,12 @@ public class MapActivity extends AppCompatActivity
     };
 
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
-    private void checkLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+    private void checkLocationPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
 
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                new AlertDialog.Builder(this)
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)){new AlertDialog.Builder(this)
                         .setTitle("Location Permission Needed")
                         .setMessage("This app needs the Location permission, please accept to use location functionality")
                         .setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -143,7 +141,7 @@ public class MapActivity extends AppCompatActivity
                         .show();
 
 
-            } else {
+            } else{
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                         MY_PERMISSIONS_REQUEST_LOCATION );
@@ -154,15 +152,14 @@ public class MapActivity extends AppCompatActivity
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
-        switch (requestCode) {
+        switch(requestCode){
             case MY_PERMISSIONS_REQUEST_LOCATION: {
                 // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
+                if(grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                     // permission was granted, yay! Do the
                     // location-related task you need to do.
-                    if (ContextCompat.checkSelfPermission(this,
+                    if(ContextCompat.checkSelfPermission(this,
                             Manifest.permission.ACCESS_FINE_LOCATION)
                             == PackageManager.PERMISSION_GRANTED) {
 
@@ -170,8 +167,8 @@ public class MapActivity extends AppCompatActivity
                         mGoogleMap.setMyLocationEnabled(true);
                     }
 
-                } else {
-                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                } else{
+                    Toast.makeText(this, "Permission denied.", Toast.LENGTH_LONG).show();
                 }
                 return;
             }
@@ -179,7 +176,7 @@ public class MapActivity extends AppCompatActivity
     }
 
     @Override
-    public void onMapLongClick(LatLng point) {
+    public void onMapLongClick(LatLng point){
         mCurrLocationMarker.remove();
         mCurrLocationMarker= mGoogleMap.addMarker((new MarkerOptions().position(point))
                 .title("Meetup Point"));
