8b3b7171a70b3ebc1c77b6e0bdcf51a8ebcba320
Jason Chu <jjchu@uwaterloo.ca>
2018-05-26 14:45:25 -0400

    Added beginning of PostService

--- airatmandroid/app/src/main/java/ca/thecashmen/airatmandroid/MapActivity.java
+++ airatmandroid/app/src/main/java/ca/thecashmen/airatmandroid/MapActivity.java
@@ -3,16 +3,19 @@ package ca.thecashmen.airatmandroid;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Looper;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -42,16 +45,25 @@ public class MapActivity extends AppCompatActivity
     Location mLastLocation;
     Marker mCurrLocationMarker;
     FusedLocationProviderClient mFusedLocationClient;
+    FloatingActionButton okayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        okayButton = findViewById(R.id.fab);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-
         mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFrag.getMapAsync(this);
+
+        okayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MapActivity.this, ConfirmationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
