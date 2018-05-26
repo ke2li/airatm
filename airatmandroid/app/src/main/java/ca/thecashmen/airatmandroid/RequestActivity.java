41baeb8b5204445c1f2f09156cd8a01746907b61
Jason Chu <jjchu@uwaterloo.ca>
2018-05-26 06:51:26 -0400

    Added RequestMoney activity.

--- airatmandroid/app/src/main/java/ca/thecashmen/airatmandroid/RequestActivity.java
+++ airatmandroid/app/src/main/java/ca/thecashmen/airatmandroid/RequestActivity.java
@@ -2,12 +2,13 @@ package ca.thecashmen.airatmandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MapActivity extends AppCompatActivity {
+public class RequestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+        setContentView(R.layout.activity_request);
     }
 }
