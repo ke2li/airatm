945027a162baba1e259496f48af9c60fcefd2b4c
David <davidzhoucanada@gmail.com>
2018-05-27 00:56:47 -0400

    added action for confirm button in confirmation activity

--- airatmandroid/app/src/main/java/ca/thecashmen/airatmandroid/ConfirmationActivity.java
+++ airatmandroid/app/src/main/java/ca/thecashmen/airatmandroid/ConfirmationActivity.java
@@ -1,13 +1,33 @@
 package ca.thecashmen.airatmandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ConfirmationActivity extends AppCompatActivity {
+
+    private Button confirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+
+        confirmButton = findViewById(R.id.confirm_button);
+
+        confirmButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast.makeText(getApplicationContext(), "Transaction confirmed!", Toast.LENGTH_LONG).show();
+                /*
+                implement saving the data of transaction
+                 */
+                Intent intent = new Intent(ConfirmationActivity.this, RequestActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
