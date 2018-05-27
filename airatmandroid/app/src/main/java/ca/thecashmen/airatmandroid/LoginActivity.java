fdab5e5021b99ec6700883ca1fc01cbae5a252e9
David <davidzhoucanada@gmail.com>
2018-05-26 20:06:21 -0400

    initalized registration activity

--- airatmandroid/app/src/main/java/ca/thecashmen/airatmandroid/LoginActivity.java
+++ airatmandroid/app/src/main/java/ca/thecashmen/airatmandroid/LoginActivity.java
@@ -1,13 +1,50 @@
 package ca.thecashmen.airatmandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button loginButton;
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        loginButton = findViewById(R.id.login_button);
+        registerButton = findViewById(R.id.register_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(!email.getText().toString().matches("") && !password.getText().toString().matches("")
+                    /*&& valid credentials*/){
+                    Intent intent = new Intent(LoginActivity.this, RequestActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(), "Invalid credentials", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
